ff333ce69efc7985f93d95872ded94b472b80ad1
Kristian Salo <salo.kristian@hotmail.com>
2019-09-08 15:29:09 +0300

    chore: add airbnb eslint config

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,7 +1,15 @@
+// Config taken from: https://codeburst.io/from-eslint-to-tslint-and-back-again-bf259c2e7437
+// See link for reference as this includes some compilaced config to get airbnb + react +
+// typescript working nicely together.
+
 module.exports = {
+  env: {
+    browser: true
+  },
   parser: '@typescript-eslint/parser',
   extends: [
-    'plugin:react/recommended',
+    'airbnb',
+    'airbnb/hooks',
     'plugin:@typescript-eslint/recommended',
     'prettier/@typescript-eslint',
     'plugin:prettier/recommended'
@@ -11,12 +19,19 @@ module.exports = {
     sourceType: 'module'
   },
   rules: {
-    'sort-keys': 'error',
-    'react/jsx-uses-react': 'error',
-    'react/jsx-uses-vars': 'error',
-    '@typescript-eslint/explicit-function-return-type': [
-      'error',
-      { allowTypedFunctionExpressions: true }
-    ]
+    'react/jsx-filename-extension': [1, { extensions: ['.jsx', '.tsx'] }],
+    'react/prop-types': 'off'
+  },
+  settings: {
+    'import/extensions': ['.js', '.jsx', '.ts', '.tsx'],
+    'import/parsers': {
+      '@typescript-eslint/parser': ['.ts', '.tsx']
+    },
+    'import/resolver': {
+      node: {
+        paths: 'src',
+        extensions: ['.js', '.jsx', '.ts', '.tsx']
+      }
+    }
   }
 };
